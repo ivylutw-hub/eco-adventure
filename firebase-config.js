bcdf69480422b4a59c4c7b90041fe46afe879c35
ivylutw-hub <ivylu.tw@gmail.com>
2026-07-26 16:33:52 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.ECO_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBYuyfK9tkoAiJze67VWfRS9npemczS_SM",
+  authDomain: "eco-adventure-v7.firebaseapp.com",
+  projectId: "eco-adventure-v7",
+  storageBucket: "eco-adventure-v7.firebasestorage.app",
+  messagingSenderId: "352564849902",
+  appId: "1:352564849902:web:203ad20d9fa955690cd7ff"
+};
